d0502470080b11b4e8e21f8c469c27c796b4141a
justinchang0630 <104087@st.fhjh.tp.edu.tw>
2021-08-15 11:28:06 +0800

    message

--- 20210815/20210815 copy 2.py
+++ 20210815/20210815 copy 2.py
@@ -3,16 +3,12 @@ a = 0
 def hi_fun():
     print("Hi singular")
     display.config(text="Hi Singular", fg= "green", bg="orange")
-def bye_fun():
-   
-    display.config(text="Hi Singular", fg= "white", bg="white")
+    a = True
 win = Tk()
 win.title("hi")
 btn1 = Button(win, text="Show screen", command=hi_fun)
 btn1.pack()
-btn2 = Button(win, text="Clear screen", command=bye_fun)
-btn2.pack()
-
+if a 
 display=Label(win,text = "hi", fg = "#588795", bg = "#00FFFF")
 display.pack()
 win.mainloop()
