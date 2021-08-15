b37f80c64f3a0ad04768fd942eaff4fbd1306dab
justinchang0630 <104087@st.fhjh.tp.edu.tw>
2021-08-15 11:27:28 +0800

    message

--- 20210815/20210815.py
+++ 20210815/20210815.py
@@ -1,1 +1,12 @@
-print( "what do you want to do today")
+from tkinter import *
+from random import *
+def hi_fun():
+    print("hello singular")
+    display.config(text="Hi Singular", fg= "green", bg="orange")
+win = Tk()
+win.title("hi")
+btn = Button(win, text="Click me", command=hi_fun)
+btn.pack()
+display=Label(win,text = "hi", fg = "#588795", bg = "#00FFFF")
+display.pack()
+win.mainloop()
