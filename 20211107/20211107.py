bd66fd30f42c5b80182b02823357bbbddf7f4ad3
justinchang0630 <104087@st.fhjh.tp.edu.tw>
2021-11-07 11:04:00 +0800

    message

--- 20211107/20211107.py
+++ 20211107/20211107.py
@@ -2,11 +2,9 @@ import requests, json
 import matplotlib.pyplot as plt
 from tkinter import *
 
-
-base_url="https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
+base_url = "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
 send_url = base_url
 print("%s\n" % send_url)
 response = requests.get(send_url)
 info = response.json()
 print(info)
-
--- main.py
+++ main.py
@@ -2,27 +2,67 @@ import requests, json
 import matplotlib.pyplot as plt
 from tkinter import *
 
+# base_url = "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
+# send_url = base_url
+# print("%s\n" % send_url)
+# response = requests.get(send_url)
+# info = response.json()
+# for i in info["retVal"]:
+#     # print(info["retVal"][i]["sarea"])
+#     if info["retVal"][i]["sarea"] == "松山區":
+#         # print(info["retVal"][i])
+#         print("地點=" + info["retVal"][i]["sna"])
+#         print("地區=" + info["retVal"][i]["sarea"])
+#         print("總車輛=" + info["retVal"][i]["tot"])
+#         print("剩餘車輛=" + info["retVal"][i]["sbi"])
+#         print()
 
-base_url="https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
-send_url = base_url
-print("%s\n" % send_url)
-response = requests.get(send_url)
-info = response.json()
+
+def get_data():
+    base_url = "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
+    response = requests.get(base_url)
+    info = response.json()
+    a_list = []
+    b_list = []
+    c_list = []
+    show = ''
+    for i in info["retVal"]:
+
+        if info["retVal"][i]["sarea"] == input_city.get():
+            print("地點=" + info["retVal"][i]["sna"])
+            print("總車輛=" + info["retVal"][i]["tot"])
+            print("剩餘車輛=" + info["retVal"][i]["sbi"])
+            a_list.append(info["retVal"][i]["sna"])
+            b_list.append(int(info["retVal"][i]["tot"]))
+            c_list.append(int(info["retVal"][i]["sbi"]))
+            show += ("地點=" + info["retVal"][i]["sna"]) + (
+                "總車輛=" + info["retVal"][i]["tot"]) + (
+                    "剩餘車輛=" + info["retVal"][i]["sbi"] + "\n")
+    print(a_list)
+    print(b_list)
+    print(c_list)
+    plt.plot(a_list, b_list, "r-^", label="總共車輛")
+    plt.plot(a_list, c_list, "g-o", label="剩餘車輛")
+    plt.legend(loc="best")
+    put_day1.config(text=show)
+
+    plt.show()
 
 
-for i in info["retVal"]:
-  print(i)
-  print(info["retVal"][i])
-  a_list.append(info["records"][i]["SiteName"])
+def clean_screen():
+    put_day1.config(text='')
 
-  print("County=" + info["records"][i]["SiteName"])
-  print("AQI=" + info["records"][i]["AQI"])
-  b_list.append(info["records"][i]["AQI"])
-                print("PM2.5=" + info["records"][i]["PM2.5"])
-                c_list.append(info["records"][i]["PM2.5"])
-                print("Status=" + info["records"][i]["Status"] + "\n")
-                print("Status=" + info["records"][i]["Status"])
-                show += "County=" + info["records"][i][
-                    "SiteName"] + " AQI=" + info["records"][i][
-                        "AQI"] + " PM2.5=" + info["records"][i][
-                            "PM2.5"] + info["records"][i]["Status"] + "\n"
+
+win = Tk()
+win.title("hi")
+
+btn1 = Button(win, text="please entry city", command=get_data)
+btn1.pack()
+btn2 = Button(win, text="clean screen", command=clean_screen)
+btn2.pack()
+input_city = Entry(win)
+input_city.pack()
+
+put_day1 = Label(win, text="", fg="#588795", bg="#00FFFF")
+put_day1.pack()
+win.mainloop()
